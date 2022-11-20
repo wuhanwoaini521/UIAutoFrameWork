914822c60e0ae7c1fab06da43a4929ca51fb0d66
wuhanwoaini521 <1319817190@qq.com>
2022-11-20 22:16:24 +0800

    update

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,3 +10,21 @@ def webDriver():
     yield driver
     #关闭driver
     driver.close()
+
+
+import allure
+from allure_commons.types import AttachmentType
+from PIL import ImageGrab
+from io import BytesIO
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport():
+    outcome = yield
+    rep = outcome.get_result()
+    if rep.when == "call" and rep.failed:
+        with BytesIO() as output:
+            img = ImageGrab.grab()
+            img.save(output, 'PNG')
+            data = output.getvalue()
+        allure.attach(data, name="异常截图", attachment_type=AttachmentType.PNG)
